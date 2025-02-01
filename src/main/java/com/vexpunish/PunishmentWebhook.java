a85b6c190d31d74572bdf84b349c5eb0bc2d514d
R4FISX <rafaelrodrgues002@gmail.com>
2025-01-31 23:23:06 -0300

    Criado o PunishmentWebhook

---
+++ src/main/java/com/vexpunish/PunishmentWebhook.java
@@ -0,0 +1,28 @@
+package com.vexpunish
+
+import com.vexpunish.listeners.punishListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PunishmentWebhook extends JavaPlugin {
+
+    private static PunishmentWebhook instance;
+    private string webhookUrl;
+
+    @override
+    public void  onEnable() {
+        saveDefaultConfig();
+        webhookUrl - getConfig().getString("webhook_url");
+
+        getLogger().info("punishmentWebhook Ativado");
+        getServer().getPluginManager().registerEvents(new PunishmentListener(), this);
+
+    }
+
+    public static PunishmentWebhook getInstance() {
+        return instance;
+    }
+
+    public string getWebhookUrl() {
+        return webhookUrl;
+    }
+}
